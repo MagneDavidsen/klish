96c27276ae3e29aefdece189e20179110abf6d6f
Magne Davidsen <magne.davidsen@gmail.com>
2014-01-05 22:19:00 +0100

    running locally

--- config.js
+++ config.js
@@ -5,6 +5,40 @@ var path = require('path'),
     config;
 
 config = {
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        // must not contain a path suffix after the hostname - "subdirs" are not (yet) supported! 
+        url: 'http://my-ghost-blog.com',
+
+        // Example mail config
+        // Visit http://docs.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
+
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        }
+    },
     production: {
         url: process.env.MY_URL,
         mail: {
